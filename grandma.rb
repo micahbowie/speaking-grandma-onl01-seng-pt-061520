faea61908ce15979bb03fdb1864beb8e04eae81d
Micah Bowie <micahbowie20@gmail.com>
2020-06-18 04:31:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def speak_to_grandma(string)
 
   if string == "I LOVE YOU GRANDMA!"
@@ -14,3 +15,14 @@ def speak_to_grandma(string)
     end
     
 end
+=======
+  if "I LOVE YOU GRANDMA!" == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif "" ==.downcase
+  puts "HUH?! SPEAK UP, SONNY!"
+elsif "Hi!" == "Hi!"
+  puts "HUH?! SPEAK UP, SONNY!"
+end 
+
+
+>>>>>>> 3549266b3c49ff29eb5c94e72dccd797d378d7a2
